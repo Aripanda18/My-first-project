c0460594c6dd4bf08f6db33ab76d04517b132a27
TK <takiralytle@gmail.com>
2018-08-24 20:55:09 -0400

    First Commit

---
+++ stalker.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+alert("That's a pretty name " + firstName);
+var age = prompt("How old are you?");
+alert("I like them a little younger than that,"+ firstName);
+console.log("Your name is " + firstName + " " + lastName );
+console.log("Your age is " + age + "years old ");
